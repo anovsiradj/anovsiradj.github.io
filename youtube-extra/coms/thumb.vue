c2fc12fe11eb973a82d94398d52a96d823ff35bb
anovsiradj <anov.siradj22@gmail.com>
2020-04-01 10:52:07 +0700

    memisahkan thumb(nail) jadi ".vue" sendiri, perlama debounce search.

---
+++ youtube-extra/coms/thumb.vue
@@ -0,0 +1,24 @@
+<template>
+	<section class="card">
+		<a v-if="snippet.thumbnails" href="#" @click.prevent="$emit('cover_click')" :title="snippet.publishedAt">
+			<img v-lazy="(snippet.thumbnails.high||snippet.thumbnails.standard||snippet.thumbnails.default).url" class="card-img-top">
+		</a>
+
+		<div v-if="snippet.title.trim().length > 0" class="card-body p-1">
+			<p class="card-title text-truncate font-weight-bold m-0">
+				<a href="#" @click.prevent="$emit('title_click')" v-text="snippet.title" :title="snippet.title"></a>
+			</p>
+		</div>
+	</section>
+</template>
+
+<script>
+module.exports = {
+	props: {
+		snippet: {
+			type: Object,
+			"default": {},
+		},
+	},
+};
+</script>
